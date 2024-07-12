b95566057177b3d2cb8f46aa49ffdfce57a96885
christopher shih <chrshih875@gmail.com>
2024-07-12 11:44:10 -0700

    setup

--- backend/recipe_manager/recipes/admin.py
+++ backend/recipe_manager/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
 # Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructions')  
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'recipe')  
